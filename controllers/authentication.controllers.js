8cbaa906c5506ffb328e56878696911837163ae4
mclbdn <michalbednar93@gmail.com>
2022-02-02 09:08:32 -0600

    Add login page

--- controllers/authentication.controllers.js
+++ controllers/authentication.controllers.js
@@ -4,6 +4,11 @@ function getRegister(req, res) {
   res.render("authentication/register");
 }
 
+function getLogin(req, res) {
+  res.render("authentication/login");
+}
+
 module.exports = {
   getRegister: getRegister,
+  getLogin: getLogin,
 };
--- routes/authentication.routes.js
+++ routes/authentication.routes.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const router = express.Router();
 
-const authenticationControllers = require("../controllers/authentication.controllers")
+const authenticationControllers = require("../controllers/authentication.controllers");
 
 router.get("/register", authenticationControllers.getRegister);
+
+router.get("/login", authenticationControllers.getLogin);
 
 module.exports = router;
