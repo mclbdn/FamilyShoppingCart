30e10d9730aa6dcf39979806944d6ccf6b7ea98d
mclbdn <michalbednar93@gmail.com>
2022-02-08 10:08:08 -0600

    Add new db connection

--- database/database.js
+++ database/database.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
 const mysql = require("mysql2");
 
-// const pool = mysql.createPool({
+// const pool = mysql.createConnection({
 //   host: "localhost",
 //   user: "root",
 //   password: process.env.DB_PASSWORD,
@@ -9,10 +9,10 @@ const mysql = require("mysql2");
 // });
 
 const pool = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: process.env.DB_PASSWORD,
-  database: "FamilyShoppingCart",
+  host: "us-cdbr-east-05.cleardb.net",
+  user: "bf645f8296fd3c",
+  password: "ca0432a7",
+  database: "heroku_15c10645fcbb5b4",
 });
 
 module.exports = pool;
